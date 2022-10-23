e20d0cf934253f14691d6f7edc7254b52aacec4f
Simthembile-Shezi <s221517979@mandela.ac.za>
2022-10-23 21:37:02 +0200

    Student receives email after registration

---
+++ Finance Tracking/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Finance_Tracking.Models
+{
+    public class ContactViewModel
+    {
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You need to enter your email address.")]
+        [StringLength(50)]
+        public string ToEmail { get; set; }
+
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Type a short message and we will get back to you.")]
+        public string Body { get; set; }
+    }
+}
